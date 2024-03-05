f9748c4c11e38fb9f31f26cf32dc7166fd586781
Bill Jones <misterunix@gmail.com>
2024-03-05 17:01:35 -0600

    Remove broadcast functionality and add error handling functions

---
+++ tools.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// CheckFatal : Check if err is set and print the error string
+// then call CleanQuit to exit the program.
+func CheckFatal(e error) {
+	if e != nil {
+		fmt.Print(e)
+		CleanQuit()
+	}
+}
+
+// CleanQuit : Call SaveJSon and exit the program.
+func CleanQuit() {
+	//SaveJSon()
+	os.Exit(0)
+}
