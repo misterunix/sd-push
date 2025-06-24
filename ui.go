82962b442e452368d7ca35d696c12b0661b1b9b7
Bill Jones <misterunix@gmail.com>
2025-06-24 07:05:07 -0500

    Refactor getmodels function to read model files and handle errors

--- ui.go
+++ ui.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
 	"github.com/rivo/tview"
 )
 
 var app *tview.Application
 var list1 *tview.List
-
+var models []string
 
 func getmodels() []string {
 	fp := "models/stable-diffusion/"
+
+
+
 	
+entries, err := os.ReadDir(fp)
+	if err != nil {
+		log.Fatalf("Error reading directory: %v", err)
+	}
+
+	fmt.Printf("Files and directories in %s:\n", dirPath)
+	for _, entry := range entries {
+		fmt.Println
+		if strings.HasSuffix(entry.Name(), ".safetensors") {
+			models = append(models, entry.Name())
+		}
+	}
+
 }
 
 func BuildUI() {
